841ed49302e8eca57ac5303bacca12ec6f13a83b
luxiaowu <xiaowu.lu@yuemia.com>
2017-08-22 15:45:49 +0800

    complete

--- blog/admin.py
+++ blog/admin.py
@@ -3,6 +3,7 @@ from models import Article
 
 class ArticleAdmin(admin.ModelAdmin):
     list_display = ('title', 'content', 'createtime')
+    list_filter = ('createtime',)
 
 # Register your models here.
 admin.site.register(Article, ArticleAdmin)
--- blog/models.py
+++ blog/models.py
@@ -7,7 +7,7 @@ from django.db import models
 class Article(models.Model):
     title = models.CharField(max_length=32, default='title')
     content = models.TextField(null=True)
-    createtime = models.DateTimeField(auto_now_add=True)
+    createtime = models.DateTimeField(null=True)
 
     def __unicode__(self):
         return self.title
